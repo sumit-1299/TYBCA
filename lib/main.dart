71228c034100f7b79d19028e4ab9a25d9ab2e325
sumit-1299 <sumitchaurasia1299@gmail.com>
2023-04-13 19:16:39 +0530

    main.dart: Test comment for github functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:register1/Screens/UI/Splash.dart';
-import 'package:firebase_core/firebase_core.dart';
-void main() async{
+import 'package:firebase_core/firebase_core.dart' ;
+import 'package:mongo_dart/mongo_dart.dart' show Db,DbCollection;
 
+class DBConnection {
+  static late DBConnection _instance;
+  final String _host = "3.106.137.118";
+  final String _port = "27017";
+  final String _dbName = "accman";
+  late Db _db;
+
+  static getInstance(){
+    if(_instance == null) {
+      _instance = DBConnection();
+    }
+    return _instance;
+  }
+  Future<Db> getConnection() async{
+        _db = Db(_getConnectionString());
+        await _db.open().then((value) {
+          print("connection Successful");
+          return value;
+        }).catchError((error) {
+          print("Error: ${error.toString()}");
+        });
+    return _db;
+  }
+
+  _getConnectionString(){
+    return "mongodb://accman:password@3.106.137.118:27017/khata?directConnection=true&appName=mongosh+1.8.0";
+  }
+  closeConnection() {
+    _db.close();
+  }
+}
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  DBConnection  abc = new DBConnection();
+      abc.getConnection();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,5 +60,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//test
 
