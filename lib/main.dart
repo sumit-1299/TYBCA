eb4bcad32a54c63f8be383b10c437bbe678051ee
ZekFurtado <furtadozek7@gmail.com>
2023-05-04 14:36:54 +0530

    main.dart: import optimization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart' ;
-import 'package:mongo_dart/mongo_dart.dart';
+import 'package:mongo_dart/mongo_dart.dart' as Mongo;
 
 import 'Home.dart';
 import 'Login.dart';
@@ -12,9 +12,11 @@ void main() async{
   //Firebase Initialization
   await Firebase.initializeApp();
   //MongoDB Initialization
-  Db _db = Db("mongodb://accman:password@3.106.137.118:27017/khata?directConnection=true&appName=mongosh+1.8.0");
+  Mongo.Db _db = Mongo.Db("mongodb://accman:password@3.106.137.118:27017/khata?directConnection=true&appName=mongosh+1.8.0");
+
   await _db.open().then((value) {
     print("connection Successful");
+    print("state: ${_db.state}, connected? ${_db.isConnected}, master: ${_db.masterConnection.connected}");
     return value;
   }).catchError((error) {
     print("Error: ${error.toString()}");
